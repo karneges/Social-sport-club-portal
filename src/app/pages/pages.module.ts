d5b2162f8402d16626b4821d9b650e5348089d44
Nikita <karneges2012@gmail.com>
2020-06-03 15:58:08 +0300

    add first page

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -5,6 +5,7 @@ import { ThemeModule } from '../@theme/theme.module';
 import { PagesComponent } from './pages.component';
 import { DashboardModule } from './dashboard/dashboard.module';
 import { PagesRoutingModule } from './pages-routing.module';
+import { MyClubGeneralComponent } from './my-club-general/my-club-general.component';
 
 @NgModule({
   imports: [
@@ -15,6 +16,7 @@ import { PagesRoutingModule } from './pages-routing.module';
   ],
   declarations: [
     PagesComponent,
+    MyClubGeneralComponent,
   ],
 })
 export class PagesModule {
